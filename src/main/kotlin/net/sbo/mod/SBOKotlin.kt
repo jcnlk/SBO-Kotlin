SkyblockOverhaul/SBO

Centralize some methods
These methods often broke in every MC update so far. We should move them to a single class so that we can update them in 1 place the next time they break.

- Added SBOKotlin#openInBrowser method to open an URL in user's preferred browser.
- Added SBOKotlin#id method to create a ResourceLocation/Identifier.
- Added SBOKotlin#toast method to add a toast message (Advancement notification in top right) to client.

src/main/kotlin/net/sbo/mod/SBOKotlin.kt
@@ -3,8 +3,11 @@ package net.sbo.mod
 import com.teamresourceful.resourcefulconfig.api.client.ResourcefulConfigScreen
 import com.teamresourceful.resourcefulconfig.api.loader.Configurator
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.Util
 import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.components.toasts.SystemToast
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.network.chat.Component
 import net.sbo.mod.compat.IrisCompatibility
 import net.sbo.mod.diana.DianaTracker
 import net.sbo.mod.utils.waypoint.WaypointManager
@@ -65,7 +68,17 @@ object SBOKotlin : ClientModInitializer {
 	lateinit var version: String
 	lateinit var mcVersion: String
 
-	fun id(path: String): ResourceLocation = ResourceLocation.fromNamespaceAndPath(MOD_ID, path)
+	fun id(path: String, owner: String = MOD_ID): ResourceLocation = ResourceLocation.fromNamespaceAndPath(owner, path)
+
+    fun toast(title: Component, message: Component) {
+        mc.toastManager.addToast(
+            SystemToast.multiline(mc, SystemToast.SystemToastId.PERIODIC_NOTIFICATION, title, message)
+        )
+    }
+
+    fun openInBrowser(url: String) {
+        Util.getPlatform().openUri(url)
+    }
 
 	override fun onInitializeClient() {
 		version = FabricLoader.getInstance().getModContainer(MOD_ID)

jcnlk/SBO-Kotlin

src/main/kotlin/net/sbo/mod/SBOKotlin.kt
@@ -3,7 +3,10 @@ package net.sbo.mod
 import com.teamresourceful.resourcefulconfig.api.client.ResourcefulConfigScreen
 import com.teamresourceful.resourcefulconfig.api.loader.Configurator
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.Util
 import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.components.toasts.SystemToast
+import net.minecraft.network.chat.Component
 import net.minecraft.resources.Identifier
 import net.sbo.mod.compat.IrisCompatibility
 import net.sbo.mod.diana.DianaTracker
@@ -65,7 +68,17 @@ object SBOKotlin : ClientModInitializer {
 	lateinit var version: String
 	lateinit var mcVersion: String
 
-	fun id(path: String): Identifier = Identifier.fromNamespaceAndPath(MOD_ID, path)
+	fun id(path: String, owner: String = MOD_ID): Identifier = Identifier.fromNamespaceAndPath(owner, path)
+
+    fun toast(title: Component, message: Component) {
+        mc.toastManager.addToast(
+            SystemToast.multiline(mc, SystemToast.SystemToastId.PERIODIC_NOTIFICATION, title, message)
+        )
+    }
+
+    fun openInBrowser(url: String) {
+        Util.getPlatform().openUri(url)
+    }
 
 	override fun onInitializeClient() {
 		version = FabricLoader.getInstance().getModContainer(MOD_ID)