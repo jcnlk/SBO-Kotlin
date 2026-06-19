SkyblockOverhaul/SBO

Very small misc
- Add userSuppliedId that catches the exception to avoid importing the exception type from outside of main class
 The exception type's name also changes between MC versions occassionally.

- Use System.currentTimeMillis() directly to avoid importing MC's Util class, which changes between MC versions.

src/main/kotlin/net/sbo/mod/SBOKotlin.kt
@@ -70,6 +70,15 @@ object SBOKotlin : ClientModInitializer {
 
 	fun id(path: String, owner: String = MOD_ID): ResourceLocation = ResourceLocation.fromNamespaceAndPath(owner, path)
 
+    fun userSuppliedId(path: String, owner: String = MOD_ID, onInvalid: (IdentifierException) -> Unit): ResourceLocation? {
+        return try {
+            id(path = path, owner = owner)
+        } catch (invalidIdentifierException: IdentifierException) {
+            onInvalid(invalidIdentifierException)
+            null
+        }
+    }
+
     fun toast(title: Component, message: Component) {
         mc.toastManager.addToast(
             SystemToast.multiline(mc, SystemToast.SystemToastId.PERIODIC_NOTIFICATION, title, message)

jcnlk/SBO-Kotlin

src/main/kotlin/net/sbo/mod/SBOKotlin.kt
@@ -3,6 +3,7 @@ package net.sbo.mod
 import com.teamresourceful.resourcefulconfig.api.client.ResourcefulConfigScreen
 import com.teamresourceful.resourcefulconfig.api.loader.Configurator
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.IdentifierException
 import net.minecraft.Util
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.components.toasts.SystemToast
@@ -69,6 +70,15 @@ object SBOKotlin : ClientModInitializer {
 	lateinit var mcVersion: String
 
 	fun id(path: String, owner: String = MOD_ID): Identifier = Identifier.fromNamespaceAndPath(owner, path)
+
+    fun userSuppliedId(path: String, owner: String = MOD_ID, onInvalid: (IdentifierException) -> Unit): Identifier? {
+        return try {
+            id(path = path, owner = owner)
+        } catch (invalidIdentifierException: IdentifierException) {
+            onInvalid(invalidIdentifierException)
+            null
+        }
+    }
 
     fun toast(title: Component, message: Component) {
         mc.toastManager.addToast(