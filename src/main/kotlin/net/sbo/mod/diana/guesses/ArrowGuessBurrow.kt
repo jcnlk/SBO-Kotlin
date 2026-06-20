a454a244f7f702b8a10c6251e6eebda7c2e06e93
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-06-20 13:36:12 +0300

    Fix Use Spade showing more often than necessary
    There is no need to use spade if arrow guess did not return null, it usually returns a precise enough location that is less than 3 blocks to the correct block, and using the spade more often causes more trouble than it solves, since precise guess contrary to the name is less precise than arrow guess.
    
    Renamed the option to "Show Use Spade on Failure" instead to match the new behavior of showing when solver returns null, instead of more than 1 result.

--- src/main/kotlin/net/sbo/mod/diana/guesses/ArrowGuessBurrow.kt
+++ src/main/kotlin/net/sbo/mod/diana/guesses/ArrowGuessBurrow.kt
@@ -251,8 +251,10 @@ object ArrowGuessBurrow {
             allGuesses.add(GuessEntry(withinRange))
         }
 
-        if (Diana.showTitleWhenInaccurate) {
-            if (withinRange.size > 1) {
+        val withinRangeFirst = withinRange.getOrNull(0)
+
+        if (Diana.showTitleWhenFailure) {
+            if (withinRangeFirst == null) {
                 if (!spadeTitleShown) BurrowDetector.requestSpade()
                 spadeTitleShown = true
             } else {
@@ -260,7 +262,7 @@ object ArrowGuessBurrow {
             }
         }
 
-        return withinRange.getOrNull(0)
+        return withinRangeFirst
     }
 
     private fun checkMoveGuess() {
